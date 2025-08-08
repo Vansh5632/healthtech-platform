28d175bded935be9cfe4ae55732a12647ad02a21
Vansh Gilhotra <vanshgilhotra8885@gmail.com>
2025-08-08 22:08:20 +0530

    feat:Added the appointments mechanism

--- apps/api/src/appointments/appointments.controller.ts
+++ apps/api/src/appointments/appointments.controller.ts
@@ -1,4 +1,49 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Request,
+  UseGuards,
+  Post,
+  Body,
+  Get,
+  Patch,
+  Param,
+} from '@nestjs/common';
+import { AppointmentsService } from './appointments.service';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+import { CreateAppointmentDto } from './dto/create-appointment.dto';
+import { JwtPayload } from 'src/auth/jwt.strategy';
+import { Appointment } from 'generated/prisma';
+import { UpdateAppointmentDto } from './dto/update-appointment.dto';
 
+@UseGuards(JwtAuthGuard)
 @Controller('appointments')
-export class AppointmentsController {}
+export class AppointmentsController {
+  constructor(private readonly appointmentsService: AppointmentsService) {}
+  @Post()
+  create(
+    @Body() createAppointmentDto: CreateAppointmentDto,
+    @Request() req: { user: JwtPayload },
+  ): Promise<Appointment> {
+    const patientId = req.user.sub;
+    return this.appointmentsService.create(createAppointmentDto, patientId);
+  }
+
+  @Get('me')
+  findAllForUser(@Request() req: { user: JwtPayload }): Promise<Appointment[]> {
+    const { sub: userId, role } = req.user;
+    return this.appointmentsService.findAllForUser(userId, role);
+  }
+  @Patch(':id/status')
+  update(
+    @Param('id') id: string,
+    @Body() UpdateAppointmentDto: UpdateAppointmentDto,
+    @Request() req: { user: JwtPayload },
+  ): Promise<Appointment> {
+    const userId = req.user.sub;
+    return this.appointmentsService.updateStatus(
+      id,
+      UpdateAppointmentDto,
+      userId,
+    );
+  }
+}
--- apps/api/src/appointments/appointments.service.ts
+++ apps/api/src/appointments/appointments.service.ts
@@ -1,4 +1,66 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ForbiddenException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { CreateAppointmentDto } from './dto/create-appointment.dto';
+import { Appointment, Role } from 'generated/prisma';
+import { UpdateAppointmentDto } from './dto/update-appointment.dto';
 
 @Injectable()
-export class AppointmentsService {}
+export class AppointmentsService {
+  constructor(private prisma: PrismaService) {}
+
+  async create(
+    CreateAppointmentDto: CreateAppointmentDto,
+    patientId: string,
+  ): Promise<Appointment> {
+    const { providerId, startTime, endTime } = CreateAppointmentDto;
+
+    return this.prisma.appointment.create({
+      data: {
+        startTime: new Date(startTime),
+        endTime: new Date(endTime),
+        patientId,
+        providerId,
+      },
+    });
+  }
+
+  async findAllForUser(userId: string, role: Role): Promise<Appointment[]> {
+    const whereClause =
+      role === Role.DOCTOR ? { providerId: userId } : { patientId: userId };
+    return this.prisma.appointment.findMany({
+      where: whereClause,
+      orderBy: { startTime: 'asc' },
+    });
+  }
+  async updateStatus(
+    appointmentId: string,
+    UpdateAppointmentDto: UpdateAppointmentDto,
+    userId: string,
+  ): Promise<Appointment> {
+    const appointment = await this.prisma.appointment.findUnique({
+      where: { id: appointmentId },
+    });
+
+    if (!appointment) {
+      throw new NotFoundException(
+        `Appointment with ID ${appointmentId} not found`,
+      );
+    }
+    if (appointment.patientId !== userId && appointment.providerId !== userId) {
+      throw new ForbiddenException(
+        'You do not have permission to update this appointment.',
+      );
+    }
+
+    return this.prisma.appointment.update({
+      where: { id: appointmentId },
+      data: {
+        status: UpdateAppointmentDto.status,
+      },
+    });
+  }
+}
