bf5e3303993cddfba3569e3d4bb72fefab7ea578
Vansh Gilhotra <vanshgilhotra8885@gmail.com>
2025-08-14 11:16:58 +0530

    feat:controller with proper rolebased access for ehr

--- apps/api/src/ehr/ehr.controller.ts
+++ apps/api/src/ehr/ehr.controller.ts
@@ -1,4 +1,42 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Param,
+  Post,
+  UseGuards,
+  Request,
+  Get,
+} from '@nestjs/common';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+import { RolesGuard } from 'src/auth/guards/roles.guard';
+import { EhrService } from './ehr.service';
+import { CreateEhrRecordDto } from './dto/create-ehr-record.dto';
+import { JwtPayload } from 'src/auth/jwt.strategy';
+import { EHRRecord } from 'generated/prisma';
+import { Roles } from 'src/auth/decorators/roles.decorator';
+import { Role } from 'generated/prisma';
 
+@UseGuards(JwtAuthGuard, RolesGuard)
 @Controller('ehr')
-export class EhrController {}
+export class EhrController {
+  constructor(private readonly ehrService: EhrService) {}
+
+  @Post()
+  @Roles(Role.DOCTOR || Role.ADMIN)
+  create(
+    @Body() createEhrRecordDto: CreateEhrRecordDto,
+    @Request() req: { user: JwtPayload },
+  ): Promise<EHRRecord> {
+    const providerId = req.user.sub;
+    return this.ehrService.create(createEhrRecordDto, providerId);
+  }
+
+  @Get('patient/:patientId')
+  findAllForPatient(
+    @Param('patientId') patientId: string,
+    @Request() req: { user: JwtPayload },
+  ): Promise<EHRRecord[]> {
+    const { sub, role } = req.user;
+    return this.ehrService.findAllForPatient(patientId, sub, role);
+  }
+}
